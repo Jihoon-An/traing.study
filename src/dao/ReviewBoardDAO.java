ecf18b1ec92fe07d13f07da0df7a06d0f7b94f44
Jihoon An <abg1418@gmail.com>
2022-09-06 17:40:09 +0900

    220906

--- src/dao/ReviewBoardDAO.java
+++ src/dao/ReviewBoardDAO.java
@@ -1,2 +1,186 @@
-package dao;public class ReviewBoardDAO {
+package dao;
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class ReviewBoardDAO {
+    private String dbID;
+    private String dbPW;
+    private String dbURL = "jdbc:oracle:thin:@localhost:1521:xe";
+
+    public String getDbID() {
+        return dbID;
+    }
+
+    public void setDbID(String dbID) {
+        this.dbID = dbID;
+    }
+
+    public String getDbPW() {
+        return dbPW;
+    }
+
+    public void setDbPW(String dbPW) {
+        this.dbPW = dbPW;
+    }
+
+    public String getDbURL() {
+        return dbURL;
+    }
+
+    public void setDbURL(String dbURL) {
+        this.dbURL = dbURL;
+    }
+
+    public ReviewBoardDAO() {
+    }
+
+    public ReviewBoardDAO(String dbID, String dbPW) {
+        this.dbID = dbID;
+        this.dbPW = dbPW;
+    }
+
+    public int insert(String... contents) {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        String sql = "insert into review_board values(review_seq.nextval, ?, ?, ?, sysdate)";
+        try (
+                Connection con = DriverManager.getConnection(dbURL, dbID, dbPW);
+        ) {
+            PreparedStatement statement = con.prepareStatement(sql);
+            int i = 1;
+            for (String content : contents) {
+                statement.setString(i++, content);
+            }
+            int result = statement.executeUpdate();
+
+            con.commit();
+
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+    public ArrayList<String> select() {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        String sql = "select POST_NO, POSTER, POST_TITLE, POST_TIME from review_board order by 1";
+        ArrayList<String> outStr = new ArrayList<>();
+        try (
+                Connection con = DriverManager.getConnection(dbURL, dbID, dbPW);
+        ) {
+            PreparedStatement statement = con.prepareStatement(sql);
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                int postNo = result.getInt("POST_NO");
+                String poster = result.getString("POSTER");
+                String postTitle = result.getString("POST_TITLE");
+                Timestamp postTimeDate  = result.getTimestamp("POST_TIME");
+
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy년 MM월 dd일 hh시 mm분");
+                String postTime = simpleDateFormat.format(postTimeDate.getTime());
+
+                outStr.add(postNo + " : " + poster + " : " + postTitle + " : " + postTime);
+            }
+            return outStr;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public ArrayList<String> select(int findPostNo) {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        ArrayList<String> outStr = new ArrayList<>();
+        String sql = "select * from review_board where post_no = ?";
+        try (
+                Connection con = DriverManager.getConnection(dbURL, dbID, dbPW);
+        ) {
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setInt(1, findPostNo);
+            ResultSet result = statement.executeQuery();
+            result.next();
+            int postNo = result.getInt("POST_NO");
+            String poster = result.getString("POSTER");
+            String postTitle = result.getString("POST_TITLE");
+            Timestamp postTimeDate  = result.getTimestamp("timestamp(POST_TIME)");
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy년 MM월 dd일 hh시 mm분");
+            String postTime = simpleDateFormat.format(postTimeDate);
+
+            outStr.add(postNo + " : " + poster + " : " + postTitle + " : " + postTime);
+            outStr.add(result.getString("POST_CONT"));
+
+            return outStr;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public int delete(int postNo) {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        String sql = "delete from review_board where post_no = ?";
+        try (
+                Connection con = DriverManager.getConnection(dbURL, dbID, dbPW);
+        ) {
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setInt(1, postNo);
+            int result = statement.executeUpdate();
+
+            con.commit();
+
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+
+    public int update(int rePostNo, String rePortCol, String rePostCon) {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        String sql = "Update review_board set ? = ? where pid = ?";
+        try (
+                Connection con = DriverManager.getConnection(dbURL, dbID, dbPW);
+        ) {
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, rePortCol);
+            statement.setString(2, rePostCon);
+            statement.setInt(3, rePostNo);
+            int result = statement.executeUpdate();
+
+            con.commit();
+
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
 }
--- src/main/Service.java
+++ src/main/Service.java
@@ -1,2 +1,151 @@
-package main;public class Service {
+package main;
+
+import dao.ReviewBoardDAO;
+import myCustom.Input;
+
+import java.util.ArrayList;
+
+public class Service {
+    private static String dbID = "review_board";
+    private static String dbPW = "review";
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("\n<< 카페 메뉴 관리 프로그램 >>");
+            System.out.println("1. 글 작성");
+            System.out.println("2. 글 목록 출력");
+            System.out.println("3. 글 내용 보기 (글 번호)");
+            System.out.println("4. 글 삭제 (글 번호)");
+            System.out.println("5. 글 수정 (글 번호)");
+            System.out.println("0. 프로그램 종료");
+            int menu = Input._integer_(0, 5);
+
+            if (menu == 1) { // 글 작성
+                ReviewBoardDAO boardDAO = new ReviewBoardDAO(dbID, dbPW);
+
+                System.out.println("작성자의 이름을 입력하세요");
+                String poster = Input._string_();
+                System.out.println("작성하실 글의 제목을 입력하세요");
+                String postTitle = Input._string_();
+                System.out.println("내용을 입력하세요");
+                String postCont = Input._string_();
+
+                int result = boardDAO.insert(poster, postTitle, postCont);
+
+                if (result > 0) {
+                    System.out.println("작성 성공");
+                } else {
+                    System.out.println("작성 실패");
+                }
+            } else if (menu == 2) { // 글 목록 출력
+                outPostList();
+
+            } else if (menu == 3) { // 글 읽기
+                int findPostNo = Input._integer_();
+                outPost(findPostNo);
+
+            } else if (menu == 4) { // 글 지우기
+                outPostList();
+                ReviewBoardDAO boardDAO = new ReviewBoardDAO(dbID, dbPW);
+                // 삭제 postNo 입력
+                System.out.println("----삭제할 글의 글 번호 입력----");
+                int delPostNo = Input._integer_();
+
+                //확인
+                outPost(delPostNo);
+                System.out.println("위 항목을 지우시겠습니까?(Y,N)");
+                String confirm = Input._string_("Y", "y", "N", "n");
+                if (confirm.equals("y") || confirm.equals("Y")) {
+                    // sql 입력
+                    int result = boardDAO.delete(delPostNo);
+
+                    if (result > 0) {
+                        System.out.println("삭제 성공!!");
+                    } else {
+                        System.out.println("삭제 대상이 없습니다.");
+                    }
+                } else {
+                    System.out.println("메인 메뉴로 돌아갑니다.");
+                }
+            } else if (menu == 5) { // 글 수정하기
+                ReviewBoardDAO boardDAO = new ReviewBoardDAO(dbID, dbPW);
+                outPostList();
+                // 수정 ID 입력
+                System.out.print("수정할 메뉴 ID 입력.");
+                int rePostNo = Input._integer_();
+                // 수정할 컬럼 내용 출력
+                outPost(rePostNo);
+
+                // 수정 컬럼 입력
+                System.out.println("수정할 컬럼");
+                System.out.println("작성자 : Poster");
+                System.out.println("제목 : Post_title");
+                System.out.println("내용 : Post_cont");
+                String rePortCol = Input._string_(
+                        "POSTER","POST_TITLE","POST_CONT",
+                        "poster", "post_title", "post_cont",
+                        "Poster", "Post_title", "Post_cont");
+
+                // 수정 내용 입력
+
+                String rePostCon = "";
+
+                if(rePortCol.equals("POST_CONT")||rePortCol.equals("post_cont")||rePortCol.equals("Post_cont")) {
+                    System.out.println("수정 내용 입력(최대 500글자)");
+                    rePostCon = Input._string_(500);
+                }else if(rePortCol.equals("POST_TITLE")||rePortCol.equals("post_title")||rePortCol.equals("Post_title")) {
+                    System.out.println("수정 제목 입력(최대 30글자)");
+                    rePostCon = Input._string_(30);
+                }else{
+                    System.out.println("수정 작성자 입력(최대 10글자)");
+                    rePostCon = Input._string_(10);
+                }
+
+                // sql 입력
+                int result = 0;
+                try {
+                    result = boardDAO.update(rePostNo, rePortCol, rePostCon);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println("같은 오류가 반복되면 관리자에게 문의하세요.");
+                }
+                if (result > 0) {
+                    System.out.println("수정 성공!!");
+                } else {
+                    System.out.println("수정 대상이 없습니다.");
+                }
+            } else {
+                System.out.println("프로그램을 종료합니다.");
+                System.exit(0);
+            }
+        }
+    }
+
+    static void outPostList() {
+        ReviewBoardDAO boardDAO = new ReviewBoardDAO(dbID, dbPW);
+        ArrayList<String> result = boardDAO.select();
+        if (result.size() == 0) {
+            System.out.println("글 목록이 존재하지 않습니다.");
+        } else {
+            System.out.println("글 번호 \t: 작성자 \t: 글 제목 \t: 작성 시간");
+            for (String str : result) {
+                System.out.println(str);
+            }
+        }
+    }
+
+    static void outPost(int findPostNo) {
+        ReviewBoardDAO boardDAO = new ReviewBoardDAO(dbID, dbPW);
+        ArrayList<String> result = boardDAO.select(findPostNo);
+        if (result == null) {
+            System.out.println("글 번호를 잘못 입력하셨습니다.");
+        }
+        else {
+            System.out.println("글 번호 \t: 작성자 \t: 글 제목 \t: 작성 시간");
+            System.out.println(result.get(0));
+            System.out.println("vv 글 내용 vv");
+            System.out.println(result.get(1));
+        }
+    }
 }
+
+
